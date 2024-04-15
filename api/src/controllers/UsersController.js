986da25111579b195a8e00e789308513d6d005e4
MrJCRJ <mr.jociriju@gmail.com>
2024-04-15 17:39:46 -0300

    Creating App Error and configuring the mandatory error

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,8 +1,14 @@
 const {hash} = require("bcryptjs")
+const AppError = require("../utils/AppError")
 
 class UsersController{
   async create(request, response) {
     const {name, email, password} = request.body
+
+
+    if (!name || !email || !password ) {
+      throw new AppError("Todos os campos precisam estar preenchidos para poder criar o cliente")
+    }
 
     const hashPassword = await hash(password, 8)
 
