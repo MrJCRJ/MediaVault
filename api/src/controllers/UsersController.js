ae51d3a116c1e11fc27b7f7e7bc8558facc20635
MrJCRJ <mr.jociriju@gmail.com>
2024-04-16 21:25:36 -0300

    user creation

--- api/src/controllers/UsersController.js
+++ api/src/controllers/UsersController.js
@@ -1,5 +1,6 @@
 const {hash} = require("bcryptjs")
 const AppError = require("../utils/AppError")
+const knex = require("../database/knex")
 
 class UsersController{
   async create(request, response) {
@@ -10,7 +11,15 @@ class UsersController{
       throw new AppError("Todos os campos precisam estar preenchidos para poder criar o cliente")
     }
 
+    const checkUserExists = await (await knex("users").where({email})).length
+
+    if(checkUserExists){
+      throw new AppError('Este email já está em uso')
+    }
+
     const hashPassword = await hash(password, 8)
+
+    await knex("users").insert({name, email, password: hashPassword})
 
     return response.status(201).json({name, email, password})
 
